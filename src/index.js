7500b7d143419b7811da0ab3d8c35041c524b6bd
perpk <k.a.perperidis@gmail.com>
2021-09-27 23:48:48 +0300

    1st draft

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import Reveal from 'reveal.js';
-import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/night.css';
-import '../img/transformers-slam-dunk.gif'
-import '../img/concept-overview.jpg'
+import 'reveal.js/plugin/highlight/monokai.css';
+import '../img/transformers-slam-dunk.gif';
+import '../img/concept-overview.jpg';
+import '../img/must-all-fmj.gif';
+import '../img/library.gif';
+import '../img/usecase.gif';
+import '../img/motivation.gif';
+import '../img/tools.gif';
+import '../img/bruce-lee.gif';
+import '../img/code-management.gif';
+import '../img/thanks.gif';
+
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm'
 
 Reveal.initialize({
     slideNumber: true,
@@ -10,5 +21,6 @@ Reveal.initialize({
     hash: true,
     history: true,
     keyboard: true,
-    touch: true
+    touch: true,
+    plugins: [RevealHighlight],
 });
